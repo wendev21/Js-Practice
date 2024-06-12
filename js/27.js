556805b646bc2258d081865705296857040fa48e
Wendev21 <Wendydev99@gmail.com>
2024-06-12 08:48:22 +0200

    POO

--- js/27.js
+++ js/27.js
@@ -13,15 +13,28 @@ const producto = {
     this.disponible = disponible;
  }
 
- const producto1 = new Producto('celular', 200, true);
- const producto2 = new Producto('Laptop', 800, false );
- const producto3 = new Producto('Mouse', 50, true);
- const producto4 = new Producto('Monitor 40"', 50, true);
+const producto1 = new Producto('celular', 200, true);
+const producto2 = new Producto('Laptop', 800, false );
+const producto3 = new Producto('Mouse', 50, true);
+const producto4 = new Producto('Monitor 40"', 50, true);
 
- 
- console.log(producto1);
+Producto.prototype.formatearProducto = function() {
+    return `El producto ${this.nombre} tiene un precio de $ ${this.precio}`;
+}
+
  console.log(producto2);
- console.log(producto3);
- console.log(producto4);
+ console.log(producto2.formatearProducto())
 
 
+ function Cliente(nombre, apellido)
+ {
+    this.nombre = nombre;
+    this.apellido = apellido;
+ }
+
+ const cliente1 = new Cliente('Maria', 'perez');
+ Cliente.prototype.formatearCliente = function() {
+    return `El Cliente ${this.nombre} ${this.apellido} ha pagado`
+ }
+console.log(cliente1);
+console.log(cliente1.formatearCliente());
