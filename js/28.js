c4a0e0a527c7b24d2eee275e4685f0ff87e9cb54
Wendev21 <Wendydev99@gmail.com>
2024-06-12 10:11:33 +0200

    Class & herencias

--- js/28.js
+++ js/28.js
@@ -17,3 +17,22 @@ const producto = new Producto("Monitor Curvo de 49'", 420);
 console.log(producto);
 console.log(producto.precioProducto());
 console.log(producto.formatearProducto());
+
+//herencia 
+
+class Libro extends Producto{
+    constructor(nombre, precio, isbn){
+        super(nombre, precio),
+        this.isbn = isbn
+    }
+    formatearProducto()
+    {
+        return `${super.formatearProducto() } y su isbn es ${this.isbn}`
+    }
+}
+
+const libro1 = new Libro("Tecnicas de js", 500, '654641514');
+console.log(libro1);
+console.log(libro1.formatearProducto());
+console.log(libro1.precioProducto());
+
