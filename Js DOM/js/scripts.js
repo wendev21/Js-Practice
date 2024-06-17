21c26c4eaae0115b210c6faa9494bab250f817f6
Wendev21 <Wendydev99@gmail.com>
2024-06-17 12:32:09 +0200

    selector

--- Js DOM/js/scripts.js
+++ Js DOM/js/scripts.js
@@ -1,10 +1,19 @@
 // querySelector
 
-const heading = document.querySelector('#heading');
-heading.textContent = 'try';
+const heading = document.querySelector('.header__texto h2');
+heading.textContent = 'Nuevo heading';
 console.log(heading);
 
 // querySelectorAll
 
+const enlaces = document.querySelectorAll('.navegacion a');
+console.log(enlaces);
+enlaces[0].textContent = 'Nuevo enlace'
+enlaces[0].href = 'https://www.google.de/?hl=de';
+enlaces[0].classList.add('nueva_clase');
+enlaces[0].classList.remove('nueva_clase');
+
 // getElementById
 
+const heading2 = document.getElementById('heading');
+console.log(heading2);
