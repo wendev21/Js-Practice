fb4cdd9cbcf77a406048c27ab3a39c3b1e278751
Wendev21 <Wendydev99@gmail.com>
2024-06-18 12:05:33 +0200

    events form

--- Js DOM/js/scripts.js
+++ Js DOM/js/scripts.js
@@ -49,3 +49,34 @@ document.addEventListener('DOMContentLoaded', function(){// only
     console.log(4);
 })
 console.log(5);
+
+window.onscroll = function(){
+    console.log('scrolling..');
+}
+
+const btnEnviar = document.querySelector('.boton--primario');
+btnEnviar.addEventListener('click', function(evento){
+    console.log(evento);
+    evento.preventDefault();
+    console.log('Enviando Formulario');
+})
+
+//Events input or textarea
+const datos = {
+    nombre : '',
+    email : '',
+    mensaje : ''
+}
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+
+
+nombre.addEventListener('input', printText);
+email.addEventListener('input', printText);
+mensaje.addEventListener('input', printText);
+
+function printText(e){
+    datos[e.target.id] = e.target.value;
+    console.log(datos);
+}
